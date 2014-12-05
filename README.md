f0489c73e8b5d2bf89cd6ff75cf4fee27219143c
Kalman Olah <kalman@inuits.eu>
2014-12-05 22:24:08 +0100

    Rewrite for BS3.0
    Signed-off-by: Kalman Olah <kalman@inuits.eu>

--- README.md
+++ README.md
@@ -1,41 +1,4 @@
 bootstrap-demo.js
 ========
 
-Unofficial extension for Twitter Bootstrap that adds 'demo' functionality.
-
-In case you're wondering what is meant by 'demo' functionality: think a transparent
-overlay and a popover. A simple way to instruct the users of your application in its
-use, now made even easier.
-
-This script supports both normal calls and activation using the data api. It requires
-only Twitter Bootstrap (including jQuery and the Twitter Bootstrap popover feature)
-to be present.
-
-It supports demo chaining, multiple demo popovers on-screen at the same time, and
-makes use of cookies to make sure these annoying things only show up once (by default).
-
-Documentation
--------------
-
-Clone the repo, cd to the directory and copy the minified script to wherever you actually need it to be. Standard practice, really.
-
-    git clone git://github.com/kalmanolah/bootstrap-demo.js.git
-    cp bootstrap-demo.js/src/bootstrap-demo.min.js /path/to/my/app/assets/js/
-
-You could also save the good people at Github some bandwidth by doing:
-
-    wget https://github.com/kalmanolah/bootstra-demo.js/raw/master/src/bootstrap-demo.min.js
-
-Please remember to make sure that jQuery and Twitter Bootstrap (including popover functionality) are present before including this script.
-
-For usage examples, visiting the [Github page](http://kalmanolah.github.com/bootstrap-demo.js/) would be your best bet.
-
-Dependencies
-------------
-
-[Twitter Bootstrap](http://twitter.github.com/bootstrap/) - Remember to include popover functionality if you're going to use a custom package.
-
-License
--------
-
-This hastily written script is MIT-licensed, but few people care. Check out the LICENSE file if you think you've got what it takes.
+https://kalmanolah.github.io/bootstrap-demo.js
--- src/bootstrap-demo.js
+++ src/bootstrap-demo.js
@@ -1,172 +1,210 @@
 /**
  * bootstrap-demo.js
  *
- * Unofficial extension for Twitter Bootstrap that adds 'demo' functionality.
- * In case you're wondering what is meant by 'demo' functionality: think a transparent
- * overlay and a popover. A simple way to instruct the users of your application in its
- * use, now made even easier.
- *
- * This script supports both normal calls and activation using the data api. It requires
- * only Twitter Bootstrap (including jQuery and the Twitter Bootstrap popover feature)
- * to be present.
- *
- * It supports demo chaining, multiple demo popovers on-screen at the same time, and
- * makes use of cookies to make sure these annoying things only show up once (by default).
- *
- * Dependencies:
- * Twitter Bootstrap (+popover functionality) - http://twitter.github.com/bootstrap/
- *
- * Usage:
- * See the Github page @ http://kalmanolah.github.com/bootstrap-demo.js/ for proper usage examples.
- *
- * Here's something simple:
- *     With the data api:
- *         <div data-provide="demo" data-id="6" data-content="<em>Demo!</em>">
- *             Lorem ipsum...
- *         </div>
- *
- *     In javascript:
- *         $('#selector').demo({
- *             id           : 'unoriginal-cookie-id',
- *             title        : 'ProTip (tm)',
- *             content      : 'Yeah... This tip's pretty pro alright.'
- *         });
- *
- * License:
- * MIT (See LICENSE)
+ * @author Kalman Olah <hello@kalmanolah.net>
+ * @license MIT
  */
-(function($){
-	// Extend jQuery with demo function
-	$.fn.demo = function(options){
-		
-		// Creates a cookie
-		function createCookie(name,value,days) {
-		    if (days) {
-		        var date = new Date();
-		        date.setTime(date.getTime()+(days*24*60*60*1000));
-		        var expires = "; expires="+date.toGMTString();
-		    }
-		    else var expires = "";
-		    document.cookie = name+"="+value+expires+"; path=/; domain="+window.location.hostname;
-		}
-		
-		// Reads a cookie
-		function readCookie(name){
-			name += '=';
-		    for (var ca = document.cookie.split(/;\s*/), i = ca.length - 1; i >= 0; i--){
-		    	if (!ca[i].indexOf(name)) return ca[i].replace(name, '');
-		    }
-		}
-		
-		// Removes a cookie
-		function removeCookie(name){
-			createCookie(name,"",-1);
-		}
-		
-		// Checks if a demo cookie is set
-		function checkDemoCookie(id){
-			return readCookie(window.location.hostname + '-demo-'+ id) != undefined;
-		}
-		
-		// Sets a demo cookie
-		function setDemoCookie(id){
-			createCookie(window.location.hostname + '-demo-' + id, 'true', 365 * 10);
-		}
-		
-		// Checks whether an element has a background of its own
-		function hasBackground(elem){
-		    var img = $(elem).css('backgroundImage'),
-		        col = $(elem).css('backgroundColor');
-		    return img != 'none' || (col != 'rgba(0, 0, 0, 0)' && col != 'transparent');
-		}
 
-	    // Set up default settings
-	    var settings = $.extend({
-	    	'overlay-style'				: 'position:fixed;top:0;left:0;bottom:0;right:0;width:100%;height:100%;background:rgba(0,0,0,0.5);z-index:1040;',
-	    	'highlight-style'			: 'z-index:1050;',
-	    	'highlight-position'		: 'position:relative;',
-	    	'highlight-background'		: 'background:rgba(255,255,255,0.8)',
-	    	'popover-style'				: 'z-index: 1060;',
-	    	'placement'					: 'top',
-	    	'title'						: 'TIP<i class="icon-bell pull-right"></i>',
-	    	'content'					: 'Click this thingy here!',
-	    	'id'						: null,
-	    	'callback'					: function(e) {
-			return;
-		},
-	    	'debug'						: false
-	    }, options);
-	    
-	    // Set up CSS rules
-	    // First, delete any existing inline styles
-	    $('style[class^="demo-style-"]').remove();
-	    $('<style class="demo-style-overlay" type="text/css">.demo-overlay{'+settings['overlay-style']+'}</style>').appendTo('head');
-	    $('<style class="demo-style-highlight" type="text/css">.demo-highlight{'+settings['highlight-style']+'}</style>').appendTo('head');
-	    $('<style class="demo-style-highlight-position" type="text/css">.demo-highlight-position{'+settings['highlight-position']+'}</style>').appendTo('head');
-	    $('<style class="demo-style-highlight-background" type="text/css">.demo-highlight-background{'+settings['highlight-background']+'}</style>').appendTo('head');
-	    $('<style class="demo-style-popover" type="text/css">.demo-popover{'+settings['popover-style']+'}</style>').appendTo('head');
-	    
-    	// Only execute if we haven't shown this demo yet
-    	if(!checkDemoCookie(settings['id']) || settings['debug']){
-    		
-    		// Set demo cookie
-    		if(!settings['debug']) setDemoCookie(settings['id']);
+Demo = function() {
+    this.showing = false;
+    this.options = {
+        id:              false,
+        persist:         true,
+        cookie_name:     '_demo',
+        cookie_days:     365,
 
-    		// Create overlay if it doesn't exist yet
-    		if($('#demo-overlay').length == 0){
-    			$('<div id="demo-overlay"></div>').appendTo('body');
-    			
-    			// Add CSS class to overlay
-    			$('#demo-overlay').addClass('demo-overlay');
-    		}
-    		
-    		// Highlight elements
-    		this.addClass('demo-highlight');
-    		// If position = static, z-index will not work, so we set the position to relative
-    		if(this.css('position') == "static") this.addClass('demo-highlight-position');
-    		// If this element has no background, we need to add one
-    		if(!hasBackground(this)) this.addClass('demo-highlight-background');
-    		
-		// Cache first of the elements
-		$that = this.first();
+        callback:        false,
+        prevent_default: true,
 
-    		// Create popover on the first of the elements
-    		$that.popover({
-    			html				: true,
-    			placement			: settings['placement'],
-    			title				: settings['title'],
-    			content				: settings['content']
-    		}).popover('show');
-    		
-    		// Set correct class on popover
-    		$('.popover').addClass('demo-popover');
-    		
-    		// Handle click
-    		this.on('click.demo.data-api', function(e){
-    			// Remove demo popovers and unbind clicks
-    			$that.popover('destroy').unbind('.demo.data-api');
-    			
-    			// Remove overlay
-    			$('#demo-overlay').remove();
+        html:            true,
+        placement:       'auto',
+        title:           '<i class="glyphicon glyphicon-bell"></i> TIP',
+        content:         'Click anywhere inside of the <span class="text-danger">highlighted</span> area to dismiss this popup.',
+        styles:          '.demo-highlight{border-color:transparent!important;background-color:#FFCC00!important;background-image:repeating-linear-gradient(45deg,transparent,transparent 35px,#FBB829 35px,#FBB829 70px)!important;}',
+    };
 
-    			// Remove highlight from elements
-    			$('.demo-highlight').removeClass('demo-highlight').removeClass('demo-highlight-background').removeClass('demo-highlight-position');
-    			
-    			// Perform callback
-    			return settings.callback(e);
-    		});
-    	}
-    	
-    	// Support chaining
-    	return this;
-	};
-	
-	$(document).ready(function(){
-		// We proudly support the data api
-		$('[data-provide="demo"]').each(function(e){
-			var $this = $(this);
-			// Perform magick
-			$this.demo($this.data());
-		});
-	});
+    /**
+     * Set a cookie to a value for a period.
+     *
+     * @param string        key   Name of cookie to set.
+     * @param mixed         value Value to set for cookie.
+     * @param integer|false days  Amount of days cookie should be valid for.
+     */
+    this.setCookie = function(key, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            var expires = "; expires="+date.toGMTString();
+        } else {
+            var expires = "";
+        }
+
+        document.cookie = key + "=" + value+expires + "; path=/; domain=" + window.location.hostname;
+    };
+
+    /**
+     * Attempt to read a cookie and return its value.
+     *
+     * @param  string          key Name of cookie to read.
+     * @param  mixed           def Default value to return.
+     * @return mixed|undefined
+     */
+    this.getCookie = function(key, def) {
+        var cookies = document.cookie.split(/;\s*/);
+        key += '=';
+
+        for (var i = cookies.length - 1; i >= 0; i--) {
+            if (!cookies[i].indexOf(key)) {
+                return cookies[i].replace(key, '');
+            }
+        }
+
+        return def;
+    };
+
+    /**
+     * Remove a cookie.
+     *
+     * @param  string key Name of cookie to remove.
+     */
+    this.removeCookie = function(key) {
+        this.setCookie(key, "", -1);
+    };
+
+    /**
+     * Return an array containing the identifiers of all seen demos.
+     *
+     * @return array
+     */
+    this.getSeenDemos = function() {
+        var cookie = this.getCookie(this.options.cookie_name, '[]');
+        return jQuery.parseJSON(cookie);
+    };
+
+    /**
+     * Set seen demos to an array containing identifiers.
+     *
+     * @param array id_array Array of demo identifiers.
+     */
+    this.setSeenDemos = function(id_array) {
+        var json = JSON.stringify(id_array);
+        this.setCookie(this.options.cookie_name, json, this.options.cookie_days);
+    };
+
+    /**
+     * Return a boolean indicating whether or not a certain demo has been seen.
+     *
+     * @param  mixed   id Identifier of the demo to check for.
+     * @return boolean
+     */
+    this.isDemoSeen = function(id) {
+        if (!this.options.persist) {
+            return false;
+        }
+
+        var seen = this.getSeenDemos();
+        return jQuery.inArray(id, seen) !== -1;
+    };
+
+    /**
+     * Mark a certain demo has been seen or not.
+     *
+     * @param  mixed id Identifier of the demo to mark as seen.
+     */
+    this.setDemoSeen = function(id) {
+        if (!this.options.persist) {
+            return false;
+        }
+
+        var seen = this.getSeenDemos();
+        seen.push(id);
+        this.setSeenDemos(seen);
+    };
+
+    this.addStyles = function() {
+        if (jQuery('style#demo-styles').length) {
+            return;
+        }
+
+        jQuery('head').append('<style id="demo-styles" type="text/css">' + this.options.styles + '</style>');
+    };
+
+    this.addStyles();
+};
+
+(function($) {
+    window.demo = new Demo();
+
+    $.fn.demo = function(options) {
+        // If we're already showing a demo, do nothing
+        if (demo.showing) {
+            return;
+        }
+
+        var options = $.extend({}, demo.options, options);
+
+        // Only execute if we haven't shown this demo yet
+        if (demo.isDemoSeen(options.id)) {
+            return this;
+        }
+
+        // Mark as seen
+        demo.setDemoSeen(options.id);
+
+        // Add highlight class
+        this.addClass('demo-highlight');
+
+        var all = this;
+        var first = this.first();
+
+        // Create popover on the first of the elements
+        first.popover({
+            html:      options.html,
+            placement: options.placement,
+            title:     options.title,
+            content:   options.content
+        }).popover('show');
+
+        // Add event blockers if needed
+        if (options.prevent_default) {
+            setTimeout(function() {
+                // $(document).on('click.demo, mousewheel.demo, DOMMouseScroll.demo, submit.demo', false);
+                $(document).on('click.demo, submit.demo', false);
+                demo.showing = true;
+            }, 0);
+        }
+
+        // Handle click
+        this.one('click', function(e) {
+            // Remove demo popover
+            first.popover('destroy');
+
+            // Remove highlight class
+            all.removeClass('demo-highlight');
+
+            // Remove event blockers
+            // $(document).off('click.demo, mousewheel.demo, DOMMouseScroll.demo, submit.demo');
+            $(document).off('click.demo, submit.demo');
+            demo.showing = false;
+
+            // Perform callback if required
+            if (options.callback) {
+                options.callback(e);
+            }
+
+            return !options.prevent_default;
+        });
+
+        return this;
+    };
+
+    // Data API support
+    $(document).ready(function() {
+        $('[data-provide="demo"]').each(function(e) {
+            var $this = $(this);
+            $this.demo($this.data());
+        });
+    });
 })(jQuery);
+
+
